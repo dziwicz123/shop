d253fc8995a87bbf227bafc4c63ac71504acf821
DESKTOP-KJ8QNG2\rafal <rafalowczarek567@gmail.com>
2024-04-18 17:57:29 +0200

    Added logo and stuff

--- app/src/HomePage.js
+++ app/src/HomePage.js
@@ -1,19 +1,92 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { MDBBtn } from 'mdb-react-ui-kit';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import AppNavbar from './Navbar'; 
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AppNavbar from "./Navbar";
+import AppFooter from "./Footer";
 import "./HomePage.css";
+
 function HomePage() {
   return (
     <>
-    <AppNavbar />
-    <div className="home-page">
-      <h1>Witaj na stronie głównej!</h1>
-      <Link to="/login"><MDBBtn color="primary">Logowanie</MDBBtn></Link>
-      <Link to="/register"><MDBBtn color="secondary">Rejestracja</MDBBtn></Link>
-    </div>
-  </>
+      <AppNavbar />
+      <div className="content">
+        <div className="page-text">
+          <p>
+            Risus tempus molestie iaculis lobortis cras euismod lectus proin
+            scelerisque sodales. A primis, nisl eros curabitur aptent fusce mus
+            mattis faucibus platea ultricies. Arcu facilisis consectetur a porta
+            nibh dolor volutpat eleifend justo. Viverra aenean ornare libero
+            nunc taciti pharetra. Luctus congue, lacus conubia cursus cras
+            nascetur quam eu. Nisl fusce vitae curabitur aenean lacus tempus?
+            Lobortis felis amet fames nascetur purus est euismod natoque eu
+            ornare. Fusce mus nullam neque curae; neque ornare mollis enim
+            vivamus. Lobortis senectus rhoncus cras porta diam. Bibendum euismod
+            class ultricies consequat natoque ridiculus aptent dictum
+            scelerisque cum phasellus. Suspendisse euismod accumsan risus porta
+            netus hendrerit. Leo tristique interdum leo mollis quam augue eu
+            tellus at nullam lobortis? Porta porta erat accumsan, mi conubia
+            congue cursus porta maecenas? Proin natoque suscipit sollicitudin
+            mauris nisi tristique. Taciti imperdiet nunc mauris.
+          </p>
+          <p>
+            Interdum pharetra eu taciti felis gravida torquent hac ligula etiam
+            netus. Dui venenatis nibh dapibus phasellus porta vestibulum congue
+            aliquet praesent aliquam vivamus. Tincidunt varius ridiculus magna
+            ullamcorper suspendisse nostra ridiculus luctus senectus. Suscipit
+            risus magna, viverra dui nunc eu nulla est vehicula habitasse
+            tortor. Nibh, bibendum sit rhoncus sem sagittis. Sed ornare aenean
+            urna ad felis. Eleifend nunc scelerisque placerat ultrices malesuada
+            eleifend maecenas et. Odio velit dictum mattis orci aptent blandit
+            lacus fermentum taciti. Auctor diam ligula nibh donec nisi
+            condimentum. Pulvinar, magnis neque ornare euismod aptent sit hac
+            adipiscing metus dis sem. Parturient primis amet ultricies velit
+            sapien ultricies lectus urna nulla. Et donec nibh cum vitae metus.
+            Mauris augue; bibendum mollis vestibulum? Libero nisi gravida est
+            fusce maecenas. Leo justo turpis.
+          </p>
+          <p>
+            Sodales leo habitasse commodo vel augue nullam, augue scelerisque.
+            Blandit ut, cum accumsan egestas euismod posuere platea hac lacinia
+            hendrerit congue felis. Litora nisl lorem primis orci vehicula
+            suspendisse eu vivamus nascetur proin maecenas quisque. Dolor fusce
+            convallis habitant felis habitant eu magna? Parturient vulputate
+            montes odio fusce turpis, fames diam potenti molestie sit nisl
+            condimentum. Platea vitae orci sociosqu himenaeos hendrerit massa
+            elit ipsum dui, fames montes! Convallis erat turpis conubia dolor
+            taciti? Pulvinar scelerisque augue nascetur natoque velit ultrices
+            iaculis lorem mi. Gravida.
+          </p>
+          <p>
+            Eu lectus convallis lectus dis cursus consectetur proin risus.
+            Turpis dui mi nunc conubia blandit eros etiam fringilla, est
+            ultricies. Tincidunt sollicitudin lectus scelerisque gravida laoreet
+            semper ipsum penatibus, taciti purus tempus. Rhoncus torquent dis
+            rhoncus. Leo id posuere tortor duis ultrices curae; elementum
+            potenti facilisis bibendum. Velit dui commodo porttitor metus
+            habitasse cum. Id, suspendisse euismod nostra et sociosqu interdum
+            scelerisque. Egestas sollicitudin nisi taciti. Posuere placerat
+            lacus nibh magna? Justo facilisi senectus feugiat sed dis curabitur
+            arcu facilisis proin quis augue potenti. Lorem ornare lobortis
+            imperdiet. Torquent arcu ullamcorper fusce sed nam varius auctor
+            platea nisi ornare integer faucibus. Molestie dapibus amet feugiat.
+            Taciti vehicula enim blandit. Potenti dapibus turpis ad.
+          </p>
+          <p>
+            Litora semper dictum netus porttitor odio nostra habitasse. Ad
+            sapien gravida senectus maecenas pharetra porttitor posuere
+            fermentum feugiat cum ultrices! Tristique vivamus justo, class
+            senectus lorem orci mi. Facilisis litora massa mattis phasellus
+            adipiscing sollicitudin netus augue vestibulum scelerisque. Tellus
+            eleifend nisi volutpat nulla sollicitudin dolor. Erat netus
+            vestibulum augue quam libero! Dui habitasse conubia aliquam risus
+            nec dictum lobortis arcu. Fames lobortis venenatis cursus pharetra
+            ultrices vel nisl magnis amet aptent. Mi senectus bibendum semper at
+            habitasse eget. Himenaeos nam ultricies erat nunc velit adipiscing
+            faucibus elit rhoncus scelerisque. Vel netus felis molestie tellus.
+          </p>
+        </div>
+      </div>
+      <AppFooter />
+    </>
   );
 }
 
--- app/src/Navbar.js
+++ app/src/Navbar.js
@@ -1,51 +1,137 @@
-import React from 'react';
-import { Navbar, Nav, NavDropdown, Form, FormControl, Button, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faShoppingCart,faSearch} from '@fortawesome/free-solid-svg-icons';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./Navbar.css"
+import React, { useState, useEffect } from "react";
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+  Container,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faUser,
+  faShoppingCart,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Navbar.css";
+
+const categories = [
+  { name: "Laptopy i Komputery", path: "/" },
+  { name: "Smartfony i Smartwatche", path: "/" },
+  { name: "Gaming i Streaming", path: "/" },
+  { name: "Podzespoły komputerowe", path: "/" },
+  { name: "Tv i Audio", path: "/" },
+];
+
 const AppNavbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY;
+      if (offset > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <Navbar style={{ background: '#2c3e50', padding: '10px 20px' }} expand="lg">
-      <Container>
-        <Navbar.Brand as={Link} to="/" style={{ color: 'white' }}>Logo</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/" style={{ color: 'white' }}>Laptopy i komputery</Nav.Link>
-            <Nav.Link as={Link} to="/" style={{ color: 'white' }}>Smartfony i smartwatche</Nav.Link>
-            <Nav.Link as={Link} to="/" style={{ color: 'white' }}>Gaming i streaming</Nav.Link>
-          </Nav>
-          <Form inline className="d-flex">
-            <FormControl
-              type="search"
-              placeholder="Czego szukasz?"
-              aria-label="Search"
-              className="mr-sm-2"
-              style={{ borderRadius: '20px' }}
-            />
-            <Button variant="primary" style={{ borderRadius: '20px' }}><FontAwesomeIcon icon={faSearch} /></Button>
-          </Form>
-          <Nav>
-            <NavDropdown 
-              title={<span style={{ color: 'white' }}><FontAwesomeIcon icon={faUser} /> Profil</span>} 
-              id="basic-nav-dropdown"
-              alignRight
-              style={{ borderRadius: '8px' }}
+    <>
+      <Navbar style={{ background: "#2c3e50", padding: "10px 20px" }} expand="lg" sticky="top">
+      <Nav className="me-auto">
+              {scrolled && (
+                <NavDropdown
+                  title={<FontAwesomeIcon icon={faBars} style={{ color: "white" }} />}
+                  id="basic-nav-dropdown"
+                >
+                  {categories.map(category => (
+                    <NavDropdown.Item key={category.name} as={Link} to={category.path}>
+                      {category.name}
+                    </NavDropdown.Item>
+                  ))}
+                </NavDropdown>
+              )}
+            </Nav>
+        <Container>
+        <Navbar.Brand as={Link} to="/" style={{ color: "white", fontWeight: "bold", display: 'flex', alignItems: 'center' }}>
+          <img src="/plus_kom_logo_light.svg" alt="Logo" style={{ maxHeight: '60px', width: 'auto' }} />
+        </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Form
+              className="d-flex"
+              style={{ flexGrow: 1, justifyContent: "center" }}
             >
-              <NavDropdown.Item as={Link} to="/login">Zaloguj się</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/register">Zarejestruj się</NavDropdown.Item>
-              <NavDropdown.Divider />
-            </NavDropdown>
-            <Nav.Link as={Link} to="/" style={{ color: 'white' }}>
-              <FontAwesomeIcon icon={faShoppingCart} /> Koszyk
-            </Nav.Link>
+              <FormControl
+                type="search"
+                placeholder="Czego szukasz?"
+                aria-label="Search"
+                className="mr-sm-2"
+                style={{ borderRadius: "20px", width: "50%", color: "black" }}
+              />
+              <Button
+                variant="outline-primary"
+                style={{ borderRadius: "20px", marginLeft: "10px" }}
+                size="lg"
+              >
+                <FontAwesomeIcon icon={faSearch} style={{ color: "white" }} />
+              </Button>
+            </Form>
+            <Nav>
+              <NavDropdown
+                title={
+                  <span>
+                    <FontAwesomeIcon icon={faUser} style={{ color: "white" }} />
+                    <span style={{ color: "white" }}> Profil</span>
+                  </span>
+                }
+                id="basic-nav-dropdown"
+              >
+                <NavDropdown.Item as={Link} to="/login">
+                  Zaloguj się
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/register">
+                  Zarejestruj się
+                </NavDropdown.Item>
+              </NavDropdown>
+
+              <Nav.Link as={Link} to="/" style={{ color: "white" }}>
+                <FontAwesomeIcon icon={faShoppingCart} /> Koszyk
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Navbar style={{ background: "grey", padding: "5px 20px" }}>
+        <Container>
+          <Nav className="mx-auto">
+            {categories.map(category => (
+              <Nav.Link
+                key={category.name}
+                as={Link}
+                to={category.path}
+                style={{ color: "white", fontSize: "16px" }}
+              >
+                {category.name}
+              </Nav.Link>
+            ))}
           </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        </Container>
+      </Navbar>
+    </>
   );
-}
+};
 
 export default AppNavbar;
