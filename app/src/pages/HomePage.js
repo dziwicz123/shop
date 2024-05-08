0774309338c22c86c6b11282dc94b30a41a19484
DESKTOP-KJ8QNG2\rafal <rafalowczarek567@gmail.com>
2024-05-08 09:39:38 +0200

    Added slider

--- app/src/pages/HomePage.js
+++ app/src/pages/HomePage.js
@@ -1,77 +1,109 @@
-import React from 'react';
-import { Container, Typography, Box } from '@mui/material';
-import AppNavbar from '../components/Navbar';
-import AppFooter from '../components/Footer';
+import React from "react";
+import { Container, Typography, Box } from "@mui/material";
+import AppNavbar from "../components/Navbar";
+import AppFooter from "../components/Footer";
+import Slider from "../components/Slider";
 
 function HomePage() {
   return (
     <>
-      <AppNavbar />
-      <Container maxWidth="md" sx={{ height: '100%', py: 3, backgroundColor: 'white' }}>
-        <Box sx={{ textAlign: 'justify' }}>
-          <Typography variant="body1" paragraph>
-            Risus tempus molestie iaculis lobortis cras euismod lectus proin scelerisque sodales. A primis...
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Interdum pharetra eu taciti felis gravida torquent hac ligula etiam netus. Dui venenatis nibh...
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Sodales leo habitasse commodo vel augue nullam, augue scelerisque...
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Eu lectus convallis lectus dis cursus consectetur proin risus. Turpis dui mi nunc conubia...
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Litora semper dictum netus porttitor odio nostra habitasse. Ad sapien gravida senectus...
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Risus tempus molestie iaculis lobortis cras euismod lectus proin scelerisque sodales. A primis...
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Interdum pharetra eu taciti felis gravida torquent hac ligula etiam netus. Dui venenatis nibh...
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Sodales leo habitasse commodo vel augue nullam, augue scelerisque...
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Eu lectus convallis lectus dis cursus consectetur proin risus. Turpis dui mi nunc conubia...
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Litora semper dictum netus porttitor odio nostra habitasse. Ad sapien gravida senectus...
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Risus tempus molestie iaculis lobortis cras euismod lectus proin scelerisque sodales. A primis...
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Interdum pharetra eu taciti felis gravida torquent hac ligula etiam netus. Dui venenatis nibh...
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Sodales leo habitasse commodo vel augue nullam, augue scelerisque...
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Eu lectus convallis lectus dis cursus consectetur proin risus. Turpis dui mi nunc conubia...
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Litora semper dictum netus porttitor odio nostra habitasse. Ad sapien gravida senectus...
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Risus tempus molestie iaculis lobortis cras euismod lectus proin scelerisque sodales. A primis...
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Interdum pharetra eu taciti felis gravida torquent hac ligula etiam netus. Dui venenatis nibh...
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Sodales leo habitasse commodo vel augue nullam, augue scelerisque...
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Eu lectus convallis lectus dis cursus consectetur proin risus. Turpis dui mi nunc conubia...
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Litora semper dictum netus porttitor odio nostra habitasse. Ad sapien gravida senectus...
-          </Typography>
-        </Box>
-      </Container>
-      <AppFooter />
+      <Box
+        sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
+      >
+        <AppNavbar />
+        <Container
+          maxWidth="lg"
+          sx={{
+            py: 3,
+            backgroundColor: "white",
+          }}
+        >
+          <Box sx={{ textAlign: "justify" }}>
+            <Slider />
+            <Typography variant="body1" paragraph>
+              Risus tempus molestie iaculis lobortis cras euismod lectus proin
+              scelerisque sodales. A primis...
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Interdum pharetra eu taciti felis gravida torquent hac ligula
+              etiam netus. Dui venenatis nibh...
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Sodales leo habitasse commodo vel augue nullam, augue
+              scelerisque...
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Eu lectus convallis lectus dis cursus consectetur proin risus.
+              Turpis dui mi nunc conubia...
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Litora semper dictum netus porttitor odio nostra habitasse. Ad
+              sapien gravida senectus...
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Risus tempus molestie iaculis lobortis cras euismod lectus proin
+              scelerisque sodales. A primis...
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Interdum pharetra eu taciti felis gravida torquent hac ligula
+              etiam netus. Dui venenatis nibh...
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Sodales leo habitasse commodo vel augue nullam, augue
+              scelerisque...
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Eu lectus convallis lectus dis cursus consectetur proin risus.
+              Turpis dui mi nunc conubia...
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Litora semper dictum netus porttitor odio nostra habitasse. Ad
+              sapien gravida senectus...
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Risus tempus molestie iaculis lobortis cras euismod lectus proin
+              scelerisque sodales. A primis...
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Interdum pharetra eu taciti felis gravida torquent hac ligula
+              etiam netus. Dui venenatis nibh...
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Sodales leo habitasse commodo vel augue nullam, augue
+              scelerisque...
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Eu lectus convallis lectus dis cursus consectetur proin risus.
+              Turpis dui mi nunc conubia...
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Litora semper dictum netus porttitor odio nostra habitasse. Ad
+              sapien gravida senectus...
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Risus tempus molestie iaculis lobortis cras euismod lectus proin
+              scelerisque sodales. A primis...
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Interdum pharetra eu taciti felis gravida torquent hac ligula
+              etiam netus. Dui venenatis nibh...
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Sodales leo habitasse commodo vel augue nullam, augue
+              scelerisque...
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Eu lectus convallis lectus dis cursus consectetur proin risus.
+              Turpis dui mi nunc conubia...
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Litora semper dictum netus porttitor odio nostra habitasse. Ad
+              sapien gravida senectus...
+            </Typography>
+          </Box>
+        </Container>
+        <AppFooter />
+      </Box>
     </>
   );
 }
