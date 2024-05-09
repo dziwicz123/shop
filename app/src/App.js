7488ec9f532be9923057ee967b0fe15ee2d43a24
blahaj <thewhiteshark@onet.pl>
2024-05-09 18:52:52 +0200

    basic category page

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import HomePage from './pages/HomePage';
+import CategoryPage from './pages/CategoryPage';
+
 function App() {
   return (
     <Router>
@@ -10,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/category" element={<CategoryPage />} />
       </Routes>
     </Router>
   );
