aef689b71e84bf00e4e4943ee936511708dc0783
dziwicz123 <michal.dziwisz@poczta.onet.pl>
2024-06-11 16:30:34 +0200

    home page connect to database

--- app/src/pages/HomePage.js
+++ app/src/pages/HomePage.js
@@ -4,30 +4,29 @@ import AppNavbar from "../components/Navbar";
 import AppFooter from "../components/Footer";
 import ProductZone from "../components/ProductZone";
 import Slider from "../components/Slider";
+
 function HomePage() {
-  return (
-    <>
-      <Box
-        sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
-      >
-        <AppNavbar />
-        <Container
-          maxWidth="lg"
-          sx={{
-            py: 3,
-            marginTop: 3,
-            marginBottom: 3,
-            borderRadius: 7,
-            backgroundColor: "white",
-          }}
-        >
-          <Slider />
-          <ProductZone />
-        </Container>
-        <AppFooter />
-      </Box>
-    </>
-  );
+    return (
+        <>
+            <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+                <AppNavbar />
+                <Container
+                    maxWidth="lg"
+                    sx={{
+                        py: 3,
+                        marginTop: 3,
+                        marginBottom: 3,
+                        borderRadius: 7,
+                        backgroundColor: "white",
+                    }}
+                >
+                    <Slider />
+                    <ProductZone />
+                </Container>
+                <AppFooter />
+            </Box>
+        </>
+    );
 }
 
 export default HomePage;
